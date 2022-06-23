ce75df96b9cab62a2df2dc49d01c8ffbb065c915
Pranav Soni <pranavsoni1101@gmail.com>
2022-06-23 11:07:31 +0530

    Solved to find  min max elements in an array using Javascript

---
+++ Arrays/minmax.js
@@ -0,0 +1,24 @@
+// Array Synatx in JS is given as:
+// array_name = [item1, item2, item3, ... ];
+let array = [50, 20, 34, 45, 89, 69, 76, 6, 72, 54];
+let n = array.length;
+
+const getMin =  (array, n) => {
+    let min = array[0];
+    for(let i = 0 ; i < n; i++){
+        min = Math.min(min, array[i])
+    }
+    return min;
+}
+
+const getMax =  (array, n) => {
+    let max = array[0];
+    for(let i = 0 ; i < n; i++){
+        max = Math.max(max, array[i])
+    }
+    return max;
+}
+
+console.log("Maximum: ", getMax(array, n) );
+console.log("Minimum: ", getMin(array, n));
+
