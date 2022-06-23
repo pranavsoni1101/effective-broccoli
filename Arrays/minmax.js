6b7be8cb1856a34073939ca5e182a0b2b39e82da
Pranav Soni <pranavsoni1101@gmail.com>
2022-06-23 11:13:59 +0530

    Added question to min max problem

--- Arrays/minmax.js
+++ Arrays/minmax.js
@@ -1,5 +1,9 @@
 // Array Synatx in JS is given as:
 // array_name = [item1, item2, item3, ... ];
+
+// Question
+// Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
 let array = [50, 20, 34, 45, 89, 69, 76, 6, 72, 54];
 let n = array.length;
 
